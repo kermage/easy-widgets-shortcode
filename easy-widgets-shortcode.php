43807e15c1c60253a37a5375329da9f6d917abde
kermage <genealyson.torcende@gmail.com>
2016-08-26 23:27:29 +0800

    update plugin info and bump version

--- easy-widgets-shortcode.php
+++ easy-widgets-shortcode.php
@@ -9,9 +9,9 @@
  * Plugin Name: Easy Widgets Shortcode
  * Plugin URI:  https://github.com/kermage/easy-widgets-shortcode/
  * Author:      Gene Alyson Fortunado Torcende
- * Author URI:  genealyson.torcende@gmail.com
- * Description: Easily embed any widget and/or sidebar to content using shortcodes
- * Version:     0.1.0
+ * Author URI:  mailto:genealyson.torcende@gmail.com
+ * Description: Easily embed any widget and/or sidebar to content using shortcodes [ews_widget] and [ews_sidebar].
+ * Version:     1.0.0
  * License:     GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ews
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /* ==================================================
 Global constants
 ================================================== */
-define( 'EWS_VERSION',  '0.1.0' );
+define( 'EWS_VERSION',  '1.0.0' );
 define( 'EWS_FILE',     __FILE__ );
 define( 'EWS_URL',      plugin_dir_url( EWS_FILE ) );
 define( 'EWS_PATH',     plugin_dir_path( EWS_FILE ) );
