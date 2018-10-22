2ba899e873137f522a7133c1586729a779b61c62
kermage <genealyson.torcende@gmail.com>
2018-10-22 22:06:53 +0800

    update license

--- easy-widgets-shortcode.php
+++ easy-widgets-shortcode.php
@@ -12,8 +12,8 @@
  * Author URI:  mailto:genealyson.torcende@gmail.com
  * Description: Easily embed any widget and/or sidebar to content using shortcodes [ews_widget] and [ews_sidebar].
  * Version:     1.0.0
- * License:     GNU General Public License v2 or later
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * License:     GNU General Public License v3.0
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: ews
  */
 
